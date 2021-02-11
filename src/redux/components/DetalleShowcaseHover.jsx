99e1fc320d7e37f46de99787ab1314c19afdfd41
Vanesa Gómez <v.cucchiararo@gmail.com>
2021-02-11 14:23:12 +0100

    styles detail component

--- src/redux/components/DetalleShowcaseHover.jsx
+++ src/redux/components/DetalleShowcaseHover.jsx
@@ -9,12 +9,22 @@ function DetalleShowcaseHover() {
         <div className="detalle-container">
           <img
             className="image_product"
+            alt="escurridor De Cubiertos"
             src="https://5.allegroimg.com/s1024/0c1916/cf4b276f4ebbacd5e09a30037965"
           ></img>
           <div className="icons-container">
-            <img className="cart-img" src={cartImage}></img>
-            <img className="eye-img" src={eyeImage}></img>
+            <img alt="icono carrito" className="cart-img" src={cartImage}></img>
+            <img
+              alt="icono vista producto"
+              className="eye-img"
+              src={eyeImage}
+            ></img>
           </div>
+        </div>
+        <div className="ranking">
+          <img className="star-ranking" alt="estrella" src={starImage}></img>
+          <img className="star-ranking" alt="estrella" src={starImage}></img>
+          <img className="star-ranking" alt="estrella" src={starImage}></img>
         </div>
         <div className="info-container">
           <h1 className="title-product">Escurridor De Cubiertos</h1>
